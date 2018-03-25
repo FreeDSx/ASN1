9163f4670fe4f8d56e0729d37df3835b268c5b06
Chad Sikorra <Chad.Sikorra@gmail.com>
2018-03-25 12:39:46 -0500

    Add a simple class for statically instantiating encoders.

---
+++ src/FreeDSx/Asn1/Encoders.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the FreeDSx ASN1 package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FreeDSx\Asn1;
+
+use FreeDSx\Asn1\Encoder\BerEncoder;
+
+/**
+ * Simple factory methods for easily getting an encoder instance for encoding / decoding.
+ *
+ * @author Chad Sikorra <Chad.Sikorra@gmail.com>
+ */
+class Encoders
+{
+    /**
+     * @param array $options
+     * @return BerEncoder
+     */
+    public static function ber(array $options = []) : BerEncoder
+    {
+        return new BerEncoder($options);
+    }
+}
---
+++ tests/spec/FreeDSx/Asn1/EncodersSpec.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the FreeDSx ASN1 package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\FreeDSx\Asn1;
+
+use FreeDSx\Asn1\Encoder\BerEncoder;
+use FreeDSx\Asn1\Encoders;
+use PhpSpec\ObjectBehavior;
+
+class EncodersSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Encoders::class);
+    }
+
+    function it_should_get_a_ber_encoder()
+    {
+        $this::ber()->shouldBeAnInstanceOf(BerEncoder::class);
+    }
+}
